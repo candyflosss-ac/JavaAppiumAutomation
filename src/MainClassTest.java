a34ea94a6af343fab614fee74d7cb31e34513d49
Alena Cherniy <alena.cherniy@genesys.com>
2025-04-06 00:02:36 +0200

    The test 2 added: testGetClassNumber to verify getClassNumber returns value > 45

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -14,4 +14,15 @@ public class MainClassTest {
 
     }
 
+    @Test
+    public void testGetClassNumber () {
+        MainClass mainClass = new MainClass();
+        int actualValue = mainClass.getClassNumber();
+        System.out.println("testGetClassNumber: actual value is " + actualValue);
+
+        Assert.assertTrue(String.format("Expected getClassNumber to return a value greater than 45, but got %s", actualValue),
+                actualValue > 45);
+
+    }
+
 }
