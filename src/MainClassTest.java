8fbe729c9ce5d433214b82abef0cabfa6f4a1519
Alena Cherniy <alena.cherniy@genesys.com>
2025-04-06 00:38:14 +0200

    The test 3 added: testGetClassString checks if it contains a substring

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class MainClassTest {
 
     @Test
-    public void testGetLocalNumber () {
+    public void testGetLocalNumber() {
         MainClass mainClass = new MainClass();
         int expectedValue = 14;
         int actualValue = mainClass.getLocalNumber();
@@ -15,13 +15,28 @@ public class MainClassTest {
     }
 
     @Test
-    public void testGetClassNumber () {
+    public void testGetClassNumber() {
         MainClass mainClass = new MainClass();
         int actualValue = mainClass.getClassNumber();
         System.out.println("testGetClassNumber: actual value is " + actualValue);
 
         Assert.assertTrue(String.format("Expected getClassNumber to return a value greater than 45, but got %s", actualValue),
                 actualValue > 45);
+
+    }
+
+    @Test
+    public void testGetClassString() {
+        MainClass mainClass = new MainClass();
+        String stringValueToCheck = mainClass.getClassString();
+        System.out.println("testGetClassString: returned value is \"" + stringValueToCheck + "\"");
+
+        boolean containsHelloLower = stringValueToCheck.contains("hello");
+        boolean containsHelloUpper = stringValueToCheck.contains("Hello");
+
+        Assert.assertTrue(
+                "The string does not contain expected substrings 'hello' or 'Hello'",
+                containsHelloLower || containsHelloUpper);
 
     }
 
